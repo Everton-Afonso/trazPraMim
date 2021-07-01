986a247a17d298a1677ab613f22dddf0ef52d438
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-07-01 10:58:33 -0300

    Conexão da pagina de cadastro de usuários como a backend

--- frontend/src/pages/Register/UserRegister/index.js
+++ frontend/src/pages/Register/UserRegister/index.js
@@ -1,11 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import api from "../../../services/api";
 
 import logo from "../../../assets/Logo.svg";
 
 import "./styles.css";
 
 export default function UserRegister() {
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [rg, setRg] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+  const [address, setAddress] = useState("");
+
+  const history = useHistory();
+
+  async function handleUserRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      cpf,
+      rg,
+      age,
+      email,
+      whatsapp,
+      city,
+      uf,
+      address,
+    };
+
+    try {
+      const response = await api.post("/createUsers", data);
+
+      alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push("/");
+    } catch (err) {
+      alert(`Erro no cadastro, tente novamente.`);
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -22,21 +62,62 @@ export default function UserRegister() {
             Já possui uma conta?<span>Clique aqui</span>
           </Link>
         </div>
-        <form>
-          <input placeholder="Nome completo" />
+        <form onSubmit={handleUserRegister}>
+          <input
+            placeholder="Nome completo"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <div className="input-group">
-            <input placeholder="Cpf" />
-            <input placeholder="Rg" />
-            <input type="number" placeholder="Idade" style={{ width: 110 }} />
+            <input
+              placeholder="Cpf"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+            />
+            <input
+              placeholder="Rg"
+              value={rg}
+              onChange={(e) => setRg(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Idade"
+              style={{ width: 110 }}
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
           </div>
-          <input type="email" placeholder="E-mail" />
-          <input placeholder="WhatsApp" />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+          />
 
           <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="Uf" style={{ width: 80 }} />
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              placeholder="Uf"
+              style={{ width: 80 }}
+              maxLength="2"
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+            />
           </div>
-          <input placeholder="Endereço" />
+          <input
+            placeholder="Endereço"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
           <button className="button" type="submit">
             Cadastrar
           </button>
